e026c86a1d3ba8632d1b12b5cf454588fa9675b5
K.Fusilier <61720744+kfusilier@users.noreply.github.com>
2022-01-23 14:31:59 -0700

    UD Challange Random Number Generator First and Last Name Insert

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
+// old
+//const React = require("react");
+// new way to do the same as above
 import React from "react";
+// old
+// const ReactDOM = require("react-dom");
+// new way to do the same as above
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+//
+const fName = "Kristin";
+const lName = "Fusilier";
+const num = 7;
+
+// can render expressions not statements
+ReactDOM.render(
+  <div>
+    <h1>
+      Hello {fName} {lName}!
+    </h1>
+    <p>Your lucky number is {Math.floor(Math.random() * 10)}.</p>
+  </div>,
+  document.getElementById("root")
+);
